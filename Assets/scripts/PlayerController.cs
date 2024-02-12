c02ff6cfd05a8e79f4c9c8dd2f9543a1dd1a9f90
Jan Snopkowski <jan.snopkowski@teb.edu.pl>
2024-02-12 10:06:46 +0100

    jazda do przodu i do tylu statku

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -15,6 +15,19 @@ public class PlayerController : MonoBehaviour
     {
         //dodaj do wspolrzednych wartosc x=1 y=0 z=0 pomnozone przez czas
         //mierzony w sekundach do ostatniej klatki
-        transform.position += new Vector3(1, 0, 0) * Time.deltaTime;
+        //transform.position += new Vector3(1, 0, 0) * Time.deltaTime;
+
+        //prezeentacja dzialania wygladzonego sterowania (emulacja joysticka)
+        //Debug.Log(Input.GetAxis("Vertical"));
+
+        //sterowanie predkoscia
+        //stworz nowy wektor przesuniecia o wartosc 1 do przodu
+        Vector3 movement = Vector3.forward;
+        //pomnoz go przez czas od ostatniej klatki
+        movement *= Time.deltaTime;
+        //pomnoz go przez "wychylenie joysticka"
+        movement *= Input.GetAxis("Vertical");
+        //dodaj ruch obiektu
+        transform.position += movement;
     }
 }
